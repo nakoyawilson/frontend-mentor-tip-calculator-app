8b92146ddceade38ab636a3996cd57ec8a10b222
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-01 11:48:33 -0400

    Use level one heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      {/* <h1 aria-label="Splitter Tip Calculator App"></h1> */}
-      <img src="/assets/images/logo.svg" alt="Splitter logo" className="logo" />
+      <h1 aria-label="Splitter Tip Calculator App">
+        <img
+          src="/assets/images/logo.svg"
+          alt="Splitter logo"
+          className="logo"
+        />
+      </h1>
     </header>
   );
 };
